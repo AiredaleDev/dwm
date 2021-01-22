48df424319cb408a66995c7d96ac0ad1ec32e049
AiredaleDev <airedaledev@protonmail.com>
2021-01-22 13:16:38 -0500

    Added alpha patch and changed color scheme

--- config.def.h
+++ config.def.h
@@ -1,27 +1,35 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no gaps with only one window */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMonoMedium Nerd Font:size=12" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#2e3440";
-static const char col_gray2[]       = "#3b4252";
-static const char col_gray3[]       = "#d8dee9";
-static const char col_gray4[]       = "#e5e9f0";
-static const char col_cyan[]        = "#81a1c1";
+static const unsigned int borderpx    = 2;        /* border pixel of windows */
+static const unsigned int snap        = 32;       /* snap pixel */
+static const unsigned int gappih      = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv      = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh      = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov      = 10;       /* vert outer gap between windows and screen edge */
+static       int smartgaps            = 0;        /* 1 means no gaps with only one window */
+static const int swallowfloating      = 0;        /* 1 means swallow floating windows by default */
+static const int showbar              = 1;        /* 0 means no bar */
+static const int topbar               = 1;        /* 0 means bottom bar */
+static const char *fonts[]            = { "JetBrainsMonoMedium Nerd Font:size=12" };
+static const char dmenufont[]         = "monospace:size=10";
+static const char background[]        = "#2e3440";
+static const char black[]             = "#3b4252";
+static const char foreground[]        = "#d8dee9";
+static const char white[]             = "#e5e9f0";
+static const char blue[]              = "#81a1c1";
+static const unsigned int baralpha    = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { foreground, background, black },
+	[SchemeSel]  = { blue,      black,       blue  },
+};
+
+static const unsigned int alphas[][3] = {
+    /*              fg       bg        border */
+    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+    [SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 static const char *const autostart[] = {
@@ -88,7 +96,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", background, "-nf", foreground, "-sb", blue, "-sf", white, NULL };
 static const char *termcmd[]    = { "alacritty", NULL };
 static const char *roficmd[]    = { "rofi", "-show", "drun", NULL };
 static const char *browsercmd[] = { "qutebrowser" };
